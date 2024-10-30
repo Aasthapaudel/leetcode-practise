1b3c42c9e12f6fa435451438b279c755ec7d85dc
Aastha <pdlpangali111@gmail.com>
2024-10-30 18:13:28 +0545

    palindrome

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -7,13 +7,15 @@
 # // @lc code=start
 class Solution:
     def isPalindrome(self, x: int) ->bool:
-        if x<0:
+        if x < 0:
             return(False)
+        
             rev_num =0
             digit =0
-            while(x// (10**digit)!=0):
-                rev_num =(rev_num * 10) + (x//(10**digit)%10)
+            
+            while(x // (10**digit) !=0):
+                rev_num = (rev_num * 10) + (x // (10**digit) % 10)
                 digit += 1
-                return(x== rev_num)
+                return(x == rev_num)
 # // @lc code=end
 
