429013009db15407ea1a75f72b200fd52649ad1d
Aastha <pdlpangali111@gmail.com>
2024-10-30 18:06:42 +0545

    palindrome

---
+++ 9.palindrome-number.py
@@ -0,0 +1,19 @@
+# /*
+#  * @lc app=leetcode id=9 lang=cpp
+#  *
+#  * [9] Palindrome Number
+#  */
+
+# // @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) ->bool:
+        if x<0:
+            return(False)
+            rev_num =0
+            digit =0
+            while(x// (10**digit)!=0):
+                rev_num =(rev_num * 10) + (x//(10**digit)%10)
+                digit += 1
+                return(x== rev_num)
+# // @lc code=end
+
